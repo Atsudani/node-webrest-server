8444381ed04bd6a69062a5310ea7b6de7dfbce35
Atsudani <atsudani@gmail.com>
2024-02-28 14:18:47 -0300

    Server modificado por segunda vez

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -45,6 +45,7 @@ export class Server {
         this.app.listen(this.port, () => {
             console.log(`Server running on port ${ this.port }`);
             console.log('Escuchando peticiones...');
+            console.log('Escuchando peticiones jeje...');
         });
     }
 }
